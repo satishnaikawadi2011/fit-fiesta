159797e74a28afa812e58cb7d699d6f5b620e4ef
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-15 12:29:37 +0530

    ✨ feat : add mongodb database connection

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 const app = express();
 
 app.get('/', (req, res) => {
 	res.send('Hello, World!');
 });
+
+mongoose.connect(process.env.MONGO_URI as any, () => console.log('Connected to MongoDB Database!!'));
 
 app.listen(5000, () => {
 	console.log('Server started on port 5000');
