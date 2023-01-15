f60fa1edb5ae8010f8d729d7fcfb887194e9576a
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-15 16:50:26 +0530

    ✨ feat : add /login route for users in backend

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,8 +1,10 @@
-import { register } from './../controllers/user';
+import { register, login } from './../controllers/user';
 import express from 'express';
 
 const router = express.Router();
 
 router.post('/register', register);
+
+router.post('/login', login);
 
 export default router;
