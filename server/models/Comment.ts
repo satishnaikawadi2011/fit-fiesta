0b3a28594a69a1e780635ee7e0823a275992c2e8
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-15 14:36:38 +0530

    ✨ feat : add Comment model to backend

---
+++ server/models/Comment.ts
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+	content:
+		{
+			type: String,
+			required: true
+		},
+	user:
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true
+		},
+	post:
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Post',
+			required: true
+		}
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
