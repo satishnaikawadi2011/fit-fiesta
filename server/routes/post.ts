c161f38ff67da1330da866be42eaa2c3b4d9d977
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-15 19:29:08 +0530

    ✨ feat : add route /post/like/:postId to like post in backend

--- server/routes/post.ts
+++ server/routes/post.ts
@@ -1,3 +1,4 @@
+import { likePost } from './../controllers/post';
 import express from 'express';
 import { createPost } from '../controllers/post';
 import { isAuthenticated } from '../middlewares/isAuthenticated';
@@ -5,5 +6,7 @@ import { isAuthenticated } from '../middlewares/isAuthenticated';
 const router = express.Router();
 
 router.post('/', isAuthenticated, createPost);
+
+router.patch('/like/:postId', isAuthenticated, likePost);
 
 export default router;
