a7dd49be95285f685649f04c06543f34aaa66a80
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-15 19:25:49 +0530

    ✨ feat : add method to like post in postController in backend

--- server/controllers/post.ts
+++ server/controllers/post.ts
@@ -17,3 +17,31 @@ export const createPost = async (req: any, res: Response) => {
 		return res.status(500).json({ message: 'Something went wrong!' });
 	}
 };
+
+export const likePost = async (req: any, res: Response) => {
+	try {
+		const { postId } = req.params;
+		const userId = req.id;
+		const post = await Post.findById(postId);
+
+		if (!post) {
+			return res.status(404).json({ message: 'Post not found!' });
+		}
+
+		const userIndex = post.likesUsers.indexOf(userId);
+		if (userIndex === -1) {
+			post.likesCount += 1;
+			post.likesUsers.push(userId);
+		}
+		else {
+			post.likesCount -= 1;
+			post.likesUsers.splice(userIndex, 1);
+		}
+
+		await post.save();
+		res.status(200).json({ post });
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ message: 'Something went wrong!' });
+	}
+};
