bc062cc99468c91e099a8de150b39795a8fd45b3
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-15 20:01:54 +0530

    ✨ feat : add /comment/:postId route to backend to comment on a post

--- server/routes/post.ts
+++ server/routes/post.ts
@@ -1,4 +1,4 @@
-import { likePost } from './../controllers/post';
+import { likePost, createComment } from './../controllers/post';
 import express from 'express';
 import { createPost } from '../controllers/post';
 import { isAuthenticated } from '../middlewares/isAuthenticated';
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post('/', isAuthenticated, createPost);
 
 router.patch('/like/:postId', isAuthenticated, likePost);
+
+router.post('/comment/:postId', isAuthenticated, createComment);
 
 export default router;
