69b380effcd1f696465b585bede751af5252d7c6
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-15 15:03:42 +0530

    ✨ feat : add error handling middlewares to backend

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import { errorHandler, notFoundRoute } from './middlewares/errorHandlers';
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send('Hello, World!');
 });
 
 mongoose.connect(process.env.MONGO_URI as any, () => console.log('Connected to MongoDB Database!!'));
+
+app.use(notFoundRoute);
+
+app.use(errorHandler);
 
 app.listen(5000, () => {
 	console.log('Server started on port 5000');
---
+++ server/middlewares/errorHandlers.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import { Error } from 'mongoose';
+
+export const notFoundRoute = (req: Request, res: Response, next: NextFunction) => {
+	const error = new Error(`Not Found - ${req.originalUrl}`);
+	res.status(404);
+	next(error);
+};
+
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+	const statusCode =
+
+			res.statusCode == 200 ? 500 :
+			res.statusCode;
+	res.status(statusCode);
+	res.json({
+		message: err.message,
+		stack:
+
+				process.env.NODE_ENV === 'production' ? null :
+				err.stack
+	});
+};
