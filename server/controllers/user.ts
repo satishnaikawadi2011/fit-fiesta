f2d925d89cfea9211b7560388a343183b08d8663
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-16 15:02:13 +0530

    ✨ feat : add method to accept connection request

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -3,6 +3,7 @@ import User, { IUser } from '../models/User';
 import jwt from 'jsonwebtoken';
 import { validateRegister } from '../validation/validateRegister';
 import { validateLogin } from '../validation/validateLogin';
+import mongoose from 'mongoose';
 
 export const register = async (req: Request, res: Response) => {
 	try {
@@ -59,9 +60,7 @@ export const makeConnectionRequest = async (req: any, res: Response) => {
 		}
 
 		const userPendingConnections = user!.pendingConnections;
-		const connectUserPendingConnections = connectUser.pendingConnections;
 		const userConnections = user!.connections;
-		const connectUserConnections = connectUser.connections;
 
 		if (userConnections.some((connection) => connection.equals(connectionId))) {
 			return res.status(409).json({ message: 'Connection already exists' });
@@ -72,11 +71,40 @@ export const makeConnectionRequest = async (req: any, res: Response) => {
 		}
 
 		userPendingConnections.push(connectionId);
-		connectUserPendingConnections.push(userId);
 		await user!.save();
 		await connectUser.save();
 
 		return res.status(200).json({ message: 'Connection request sent' });
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ message: 'Something went wrong!' });
+	}
+};
+
+export const acceptConnection = async (req: any, res: Response) => {
+	try {
+		const userId = req.id;
+		const connectionId = req.params.connectionId;
+
+		const currentUser = await User.findById(userId);
+		if (!currentUser) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+		const connection = await User.findById(connectionId);
+		if (!connection) {
+			return res.status(404).json({ message: 'Connection not found' });
+		}
+		if (!currentUser.pendingConnections.includes(connection._id)) {
+			return res.status(404).json({ message: 'Connection request not found' });
+		}
+		currentUser.pendingConnections = currentUser.pendingConnections.filter((conn: mongoose.Types.ObjectId) => {
+			return conn.toString() !== connection._id.toString();
+		});
+		currentUser.connections.push(connection._id);
+		connection.connections.push(currentUser._id);
+		await currentUser.save();
+		await connection.save();
+		return res.json({ currentUser });
 	} catch (err) {
 		console.log(err);
 		return res.status(500).json({ message: 'Something went wrong!' });
