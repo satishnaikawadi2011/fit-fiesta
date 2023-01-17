e61a6d3024ebf4e6f92dd8798ec25617e7d5e233
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-17 18:36:22 +0530

    ✨ feat : complete register page UI

--- client/src/components/form/AppFormField.tsx
+++ client/src/components/form/AppFormField.tsx
@@ -1,13 +1,24 @@
-import { FormControl, FormHelperText, FormLabel, Input, InputProps } from '@chakra-ui/react';
+import { FormControl, FormHelperText, FormLabel, Input, InputGroup, InputProps } from '@chakra-ui/react';
 import { useFormikContext } from 'formik';
 import React from 'react';
 
 interface Props {
 	label: string;
 	name: string;
+	withIcon?: boolean;
+	iconSide?: 'left' | 'right';
+	iconElement?: any;
 }
 
-const AppFormField: React.FC<InputProps & Props> = ({ label, name, isRequired, ...props }) => {
+const AppFormField: React.FC<InputProps & Props> = ({
+	label,
+	name,
+	isRequired,
+	withIcon = false,
+	iconElement,
+	iconSide = 'left',
+	...props
+}) => {
 	const { errors, touched, setFieldTouched, values, setFieldValue } = useFormikContext();
 	let formErrors: any = errors;
 	let formTouched: any = touched;
@@ -16,14 +27,27 @@ const AppFormField: React.FC<InputProps & Props> = ({ label, name, isRequired, .
 	return (
 		<FormControl marginBottom={5} isInvalid={formErrors[name] && formTouched[name]} isRequired={isRequired}>
 			<FormLabel htmlFor={name}>{label}</FormLabel>
-			<Input
-				id={name}
-				name={name}
-				onChange={(e) => setFieldValue(name, e.target.value)}
-				value={myValues[name]}
-				onBlur={() => setFieldTouched(name)}
-				{...props}
-			/>
+			{
+				!withIcon ? <Input
+					id={name}
+					name={name}
+					onChange={(e) => setFieldValue(name, e.target.value)}
+					value={myValues[name]}
+					onBlur={() => setFieldTouched(name)}
+					{...props}
+				/> :
+				<InputGroup>
+					{iconSide == 'left' && iconElement}
+					<Input
+						id={name}
+						name={name}
+						onChange={(e) => setFieldValue(name, e.target.value)}
+						value={myValues[name]}
+						onBlur={() => setFieldTouched(name)}
+						{...props}
+					/>
+					{iconSide == 'right' && iconElement}
+				</InputGroup>}
 			{formErrors[name] && formTouched[name] && <FormHelperText color={'red'}>{formErrors[name]}</FormHelperText>}
 		</FormControl>
 	);
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,9 +1,22 @@
-import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
+import {
+	Box,
+	Button,
+	Flex,
+	Heading,
+	HStack,
+	InputRightElement,
+	Link,
+	Stack,
+	useColorModeValue,
+	Text
+} from '@chakra-ui/react';
 import { Formik } from 'formik';
 import { FormikProps } from 'formik/dist/types';
-import React from 'react';
+import { useState } from 'react';
 import * as Yup from 'yup';
 import AppFormField from '../components/form/AppFormField';
+import Logo from '../components/Logo';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
 interface FormValues {
 	username: string;
@@ -37,35 +50,131 @@ const validationSchema = Yup.object({
 	fullName: Yup.string().required('fullName is a required field !'),
 	weight:
 		Yup.number()
-			.required('Weight is required')
+			.required('Weight is a required field')
 			.min(0, 'Weight must be greater than 0')
 			.max(1000, 'Weight is not realistic')
 			.positive('Weight must be a positive number'),
 	height:
 		Yup.number()
-			.required('Height is required')
+			.required('Height a is required field')
 			.min(0, 'Height must be greater than 0')
 			.max(300, 'Height is not realistic')
 			.positive('Height must be a positive number'),
 	targetWeight:
 		Yup.number()
-			.required('targetWeight is required')
+			.required('targetWeight is a required field')
 			.min(0, 'targetWeight must be greater than 0')
 			.max(1000, 'targetWeight is not realistic')
 			.positive('targetWeight must be a positive number')
 });
 
 const RegisterPage = () => {
-	const handleSubmit = () => {};
+	const [
+		showPassword,
+		setShowPassword
+	] = useState(false);
+	const onSubmit = (values: FormValues) => {
+		console.log(values);
+	};
 
 	return (
 		<div>
-			<Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-				{({  }: FormikProps<FormValues>) => {
+			<Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+				{({ handleSubmit }: FormikProps<FormValues>) => {
 					return (
-						<form>
-							<AppFormField isRequired label="Full Name" name="fullName" />
-						</form>
+						<Flex
+							minH={'100vh'}
+							align={'center'}
+							justify={'center'}
+							bg={useColorModeValue('gray.50', 'gray.800')}
+						>
+							<Stack mx={'auto'} maxW={'lg'} py={12} px={6}>
+								<Stack align={'center'}>
+									<HStack>
+										<Logo width={'5rem'} height={'5rem'} fill="#A2D2FF" />
+										<Heading
+											fontSize={'4xl'}
+											fontStyle="italic"
+											color={'primary.200'}
+											textAlign={'center'}
+										>
+											FitFiesta
+										</Heading>
+									</HStack>
+									<Heading fontSize={'4xl'} textAlign={'center'}>
+										Sign up
+									</Heading>
+									<Text fontSize={'lg'} color={'gray.600'}>
+										to enjoy all of our cool features ✌️
+									</Text>
+								</Stack>
+								<Box
+									w={500}
+									rounded={'lg'}
+									bg={useColorModeValue('white', 'gray.700')}
+									boxShadow={'lg'}
+									p={8}
+								>
+									<Stack>
+										<AppFormField isRequired label="Full Name" name="fullName" />
+										<AppFormField isRequired label="Email" name="email" />
+										<AppFormField isRequired label="Username" name="username" />
+										<AppFormField
+											isRequired
+											label="Password"
+											name="password"
+											type={
+
+													showPassword ? 'text' :
+													'password'
+											}
+											withIcon
+											iconSide="right"
+											iconElement={
+												<InputRightElement h={'full'}>
+													<Button
+														variant={'ghost'}
+														onClick={() => setShowPassword((showPassword) => !showPassword)}
+													>
+														{
+															showPassword ? <ViewIcon /> :
+															<ViewOffIcon />}
+													</Button>
+												</InputRightElement>
+											}
+										/>
+										<AppFormField isRequired label="Location" name="location" />
+										<AppFormField isRequired label="Weight" name="weight" type="number" />
+										<AppFormField isRequired label="Height" name="height" type="number" />
+										<AppFormField
+											isRequired
+											label="Target Weight"
+											name="targetWeight"
+											type="number"
+										/>
+										<Stack spacing={10} pt={2}>
+											<Button
+												loadingText="Submitting"
+												size="lg"
+												bg={'primary.300'}
+												color={'white'}
+												_hover={{
+													bg: 'primary.400'
+												}}
+												onClick={handleSubmit as any}
+											>
+												Sign up
+											</Button>
+										</Stack>
+										<Stack pt={6}>
+											<Text align={'center'}>
+												Already a user? <Link color={'primary.400'}>Login</Link>
+											</Text>
+										</Stack>
+									</Stack>
+								</Box>
+							</Stack>
+						</Flex>
 					);
 				}}
 			</Formik>
