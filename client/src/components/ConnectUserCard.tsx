a03ea842ac074ca88d872f16f11c68c9ba02fb01
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-02-03 10:19:41 +0530

    ✨ feat : add ConnectUserCard component

---
+++ client/src/components/ConnectUserCard.tsx
@@ -0,0 +1,104 @@
+import {
+	Flex,
+	Avatar,
+	Heading,
+	Box,
+	Image,
+	Text,
+	Link,
+	Center,
+	Button,
+	Skeleton,
+	SkeletonCircle
+} from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import useApi from '../hooks/useApi';
+import userApi from '../api/user';
+import { IUser } from '../types/User';
+import MutualConnsModel from './MutualConnsModel';
+
+interface Props {
+	user: IUser;
+}
+
+const ConnectUserCard: React.FC<Props> = ({ user }) => {
+    const { _id, fullName, username, profileImg, coverImg } = user;
+    
+    const { data: mutualConnsData, error, errorMsg, loading, request: getMutualConnsReq } = useApi(
+		userApi.getMutualConnections
+    );
+    
+    const mutualConns: IUser[] = (mutualConnsData as any)?.data.mutualConnections
+    
+    const [showConnsModal, setShowConnsModal] = useState(false)
+
+	useEffect(() => {
+		getMutualConnsReq(_id);
+    }, []);
+    
+    if (loading) return <SkeletonComponent/>
+
+	return (
+		<React.Fragment>
+			<MutualConnsModel isOpen={showConnsModal} onClose={() => setShowConnsModal(false)} name={fullName} mutualConns={mutualConns} />
+			<Box maxW={'230px'} w={'full'} bg={'gray.100'} boxShadow={'lg'} rounded={'md'} overflow={'hidden'}>
+				<Image h={'100px'} w={'full'} src={coverImg} objectFit={'cover'} />
+				<Flex justify={'center'} mt={-12}>
+					<Avatar
+						size={'xl'}
+						src={profileImg}
+						name={fullName}
+						css={{
+							border: '2px solid white'
+						}}
+					/>
+				</Flex>
+
+				<Box>
+					<Center flexDirection={'column'}>
+						<Heading fontSize={'xl'} fontWeight={500} fontFamily={'body'}>
+							{fullName}
+						</Heading>
+						<Text color={'gray.500'}>@{username}</Text>
+						{mutualConns.length !== 0 && <Link my={2} color={'gray.400'} fontWeight={'light'} fontSize={'sm'}>
+							{getMutualConnStr(mutualConns)}
+						</Link>}
+						<Button my={4} variant={'outline'} colorScheme="primary">
+							Connect
+						</Button>
+					</Center>
+				</Box>
+			</Box>
+		</React.Fragment>
+	);
+};
+
+const getMutualConnStr = (conns: IUser[]) => {
+    if (conns.length === 1) return `1 mutual connection`;
+    
+    return `${conns.length} mutual connections`
+}
+
+const SkeletonComponent = () => {
+	return (
+		<React.Fragment>
+			<Box maxW={'230px'} w={'full'} bg={'gray.100'} boxShadow={'lg'} rounded={'md'} overflow={'hidden'}>
+				<Skeleton h={'100px'} w={'full'} />
+				<Flex justify={'center'} mt={-12}>
+					<SkeletonCircle width={90} height={90} />
+				</Flex>
+
+				<Box>
+					<Center width={'100%'} flexDirection={'column'}>
+						<Skeleton height={4} my={2} width="60%" />
+						<Skeleton height={3} width="50%" mb={2} />
+						<Skeleton height={2} width="70%" />
+						<Skeleton my={5} rounded="full" height={8} width="50%" mr={3} />
+					</Center>
+				</Box>
+			</Box>
+		</React.Fragment>
+	);
+};
+
+export default ConnectUserCard;
