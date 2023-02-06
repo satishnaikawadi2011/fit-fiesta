40b6aa19c4511b99f11f944fa587c173f97c0219
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-02-06 13:15:34 +0530

    ✨ feat : add basic notification types and messages

--- server/models/Notification.ts
+++ server/models/Notification.ts
@@ -1,20 +1,47 @@
 import * as mongoose from 'mongoose';
+import { IUser } from './User';
 const Schema = mongoose.Schema;
+
+export type NotificationType =
+	| 'connection_request_made'
+	| 'connection_request_accepted'
+	| 'connection_request_rejected'
+	| 'removed_from_connection'
+	| 'connection_request_withdrawn';
+
+export const getNotificationMessage = (type: NotificationType, user: IUser) => {
+	switch (type) {
+		case 'connection_request_made':
+			return `@${user.username} has sent you a connection request.`;
+		case 'connection_request_accepted':
+			return `Your connection request with @${user.username} has been accepted.`;
+		case 'connection_request_rejected':
+			return `Your connection request with @${user.username} has been rejected.`;
+		case 'removed_from_connection':
+			return `You have been removed from @${user.username}'s connections.`;
+		case 'connection_request_withdrawn':
+			return `@${user.username} has withdrawn their connection request.`;
+	}
+};
 
 export interface INotification {
 	message: string;
-	recipient: string;
+	recipients: string[];
 	read: boolean;
+	type: NotificationType;
+	image: string;
 }
 
 const NotificationSchema = new Schema(
 	{
-		recipient:
-			{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: 'User',
-				required: true
-			},
+		recipients:
+			[
+				{
+					type: mongoose.Schema.Types.ObjectId,
+					ref: 'User',
+					required: true
+				}
+			],
 		message:
 			{
 				type: String,
@@ -26,6 +53,11 @@ const NotificationSchema = new Schema(
 				default: false
 			},
 		image:
+			{
+				type: String,
+				required: true
+			},
+		type:
 			{
 				type: String,
 				required: true
