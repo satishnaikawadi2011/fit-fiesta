df3a9aeee11fc02c64181cc22586db1733316790
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-02-17 02:24:41 +0530

    ✨ feat : add SentGroupJoinRequests component

---
+++ client/src/components/my-network/SentGroupJoinRequests.tsx
@@ -0,0 +1,62 @@
+import { Box, Center, Divider, Spinner, Text } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import userApi from '../../api/user';
+import { setSentGroupRequests } from '../../app/features/user';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import useApiUpdated from '../../hooks/useApiUpdated';
+import { IGroup } from '../../types/Group';
+import SentGroupJoinRequestItem from './SentGroupJoinRequestItem';
+
+const SentGroupJoinRequests = () => {
+	const { sentGroupRequests } = useAppSelector((state) => state.user);
+	const dispatch = useAppDispatch();
+	const { data, error, loading, request: getSentJoinGrpReqs } = useApiUpdated<{ sentGroupJoinRequests: IGroup[] }>(
+		userApi.getSentConnectionsRequests
+	);
+
+	useEffect(() => {
+		getSentJoinGrpReqs();
+	}, []);
+
+	useEffect(
+		() => {
+			if (data) {
+				dispatch(setSentGroupRequests(data.sentGroupJoinRequests));
+			}
+		},
+		[
+			data
+		]
+	);
+
+	if (loading)
+		return (
+			<Center>
+				<Spinner colorScheme={'primary'} size={'lg'} />
+			</Center>
+		);
+
+	return (
+		<Box boxShadow="md" bg={'gray.100'} width={'100%'} mb={5} roundedTop={'lg'}>
+			<Text p={5}>Sent Join Group Requests</Text>
+			<Divider />
+			<Box p={5}>
+				{sentGroupRequests.map((req) => {
+					return (
+						<React.Fragment key={req._id}>
+							<SentGroupJoinRequestItem group={req} />
+							<Divider />
+						</React.Fragment>
+					);
+				})}
+			</Box>
+			{sentGroupRequests.length === 0 && (
+				<Box height={'calc(100vh - 15rem)'} width={'100%'}>
+					<Center height={'100%'}>No requests</Center>
+				</Box>
+			)}
+		</Box>
+	);
+};
+
+export default SentGroupJoinRequests;
